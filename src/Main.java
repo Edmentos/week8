ce66659a33d1d87cc3f68013ea0b8b09ed86c192
Edmentos <eddiewilkinson000@gmail.com>
2023-11-20 17:30:46 +0000

    added printwriter

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
-
+import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args) {
         FileWriter myFile;
+        String filename = "filegoober.txt";
 
-        {
-            try {
-                myFile = new FileWriter("Filegoober.txt");
-                myFile.write("Good Evening!");
-                myFile.append("\nEdmond fr");
-                myFile.close();
-            } catch (IOException e) {
-                System.out.println("An error has occured");
-            }
+
+        /*   try {
+               myFile = new FileWriter("Filegoober.txt");
+               myFile.append("Edmond fr fr fr");
+               myFile.close();
+           } catch (IOException e) {
+               System.out.println("An error has occured");
+           }*/
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true)){
+
         }
+        catch (IOException e) {
 
-    }
+        }
 }
